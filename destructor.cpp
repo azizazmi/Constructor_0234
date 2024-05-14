9228f525d90bdd28c2deab2235c65319daee4879
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-05-14 15:42:59 +0700

    menambahkan class angka

--- destructor.cpp
+++ destructor.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class angka {
+private:
+    int *arr;
+    int panjang;
+public:
+    angka(int);  //constructor
+    ~angka();   //destructor
+    void cetakData();
+    void isiData;
+};
