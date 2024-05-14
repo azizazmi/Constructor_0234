3fe0d7de29c5857fbdaa92db7f1270fc94d6be60
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-05-14 16:14:06 +0700

    menambahkan fungsi main

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -13,5 +13,12 @@ public:
     {
         return this->judul;
     }
-    
+
 } bukunya;
+
+int main() {
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul();  //chain function calls
+    return 0;
+}
